f23a98c235d9e70b94846822220085447d3a408d
Igor Pereira <igorpereira15@gmail.com>
2019-05-27 19:30:13 +0200

    Add folder for tests, and a test for the pooling system.

---
+++ Assets/Tests/Pooling/PoolTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FenrirPack.Pooling;
+
+public class PoolTest : MonoBehaviour
+{
+	[SerializeField]
+	PooledObject prefab;
+
+	Pool pool;
+
+	PooledObject instance;
+
+	private void Start()
+	{
+		pool = Pool.CreatePool(prefab, 10, 5);
+
+		instance = pool.Spawn(this.transform);
+
+		Invoke("Despawn", 2);
+	}
+
+	void Despawn()
+	{
+		instance.Despawn();
+	}
+}
